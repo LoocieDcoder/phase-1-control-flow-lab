abf882bc6d35737fcda5c759774d0a697ab2ba38
Lucy <quaysonlucyofficial@gmail.com>
2022-05-12 09:22:25 +0100

    Assignment completed

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+const feet = 400;
+
+let scuberGreetingForFeet; 
+
+if scuberGreetingForFeet(feet === 400){
+  'gives customers a free sample if the ride is less than or equal to 400 feet'
+}
+else if scuberGreetingForFeet(feet === 199){
+  'This is on me'
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+const feet = 400;
+const scuberGreetingForFeet = feet >= 400 ? 'gives customers a free sample if the ride is less than or equal to 400 feet' : 'This is on me'
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+
+const feet(400)
+  let scuberGreetingForFeet;
+  if (feet === 400) {
+    scuberGreetingForFeet 'gives customers a free sample if the ride is less than or equal to 400 feet'
+  } else if (feet === 199){
+    scuberGreetingForFeet = "This is on me"
+  }
+
