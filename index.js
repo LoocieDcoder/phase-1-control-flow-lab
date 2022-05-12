f0941af0946998966c5646cbf38c203a6650be68
Lucy <quaysonlucyofficial@gmail.com>
2022-05-12 19:16:25 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,23 +1,7 @@
-const feet = 400;
-
-let scuberGreetingForFeet; 
-
-if scuberGreetingForFeet(feet === 400){
-  'gives customers a free sample if the ride is less than or equal to 400 feet'
-}
-else if scuberGreetingForFeet(feet === 199){
-  'This is on me'
+function scuberGreetingForFeet(400){
+  console.log('gives customers a free sample if the ride is less than or equal to 400 feet');
 }
 
-const feet = 400;
-const scuberGreetingForFeet = feet >= 400 ? 'gives customers a free sample if the ride is less than or equal to 400 feet' : 'This is on me'
-
-
-const feet(400)
-  let scuberGreetingForFeet;
-  if (feet === 400) {
-    scuberGreetingForFeet 'gives customers a free sample if the ride is less than or equal to 400 feet'
-  } else if (feet === 199){
-    scuberGreetingForFeet = "This is on me"
-  }
-
+function scuberGreetingForFeet(199){
+  console.log('This one is on me!');
+}
